cd0ea0abb9686c1637c1582f337f481fc931c228
Natalia Luiza <natalialuizas@live.com>
2020-06-08 17:22:45 -0300

    adicionando scroll ao parceiros

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+
+$(function(){
+  $(".watch-slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    prevArrow: $("#arrow-prev"),
+    nextArrow: $("#arrow-next"),
+  });
+
+
+})
